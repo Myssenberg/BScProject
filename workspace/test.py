bf87d276313c45a547196b625bf7880049c0409e
Myssenberg <myje@itu.dk>
2023-03-24 13:31:17 +0100

    first working proof in test.py

--- workspace/test.py
+++ workspace/test.py
@@ -1,2 +1,25 @@
 import zksk;
+import petlib.bn as bn;
+import petlib.ec as ec;
+from petlib.ec import EcGroup
 
+#I know 'a' such that 'g^a mod p = b'
+"""
+p = 7
+g = 3
+a = zksk.Secret(2)
+r = zksk.Secret(4)
+
+com = g**r
+
+stmt = zksk.DLRep(com, g**x, )
+
+"""
+x = zksk.Secret(name="x")
+g = EcGroup().generator()
+y = 42 * g
+stmt = zksk.DLRep(y, x * g)
+nizk = stmt.prove({x: 42})
+v = stmt.verify(nizk)
+
+print(v)
