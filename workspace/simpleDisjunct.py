8b610c69e65a683940a309c652b8c009073263e4
Myssenberg <myje@itu.dk>
2023-03-29 15:57:26 +0200

    very simple disjoint working

--- workspace/simpleDisjunct.py
+++ workspace/simpleDisjunct.py
@@ -1,25 +1,28 @@
 from zksk import Secret, DLRep
+from zksk.composition import OrProofStmt
 import zksk
 import petlib.bn as bn
 import petlib.ec as ec
 from petlib.ec import EcGroup
 
-g = EcGroup().generator()
-print(g)
-x0 = Secret(value = 20)
-x1 = Secret()
+group = EcGroup()
 
-y0 = x0 * g
-y1 = x1 * g
+g0 = group.hash_to_point(b"one")
+g1 = group.hash_to_point(b"two")
 
-stmt0 = DLRep(y0, x0 * g)
-stmt1 = DLRep(y1, x1 * g)
-stmt1.set_simulated()
+x0 = Secret(value=3)
+x1 = Secret(value=40)
 
-stmt = zksk.composition.OrProofStmt(stmt0, stmt1)
+y0 = x0.value * g0
+y1 = x1.value * g1
 
-nizk = stmt.prove()
+stmt = DLRep(y0, x0 * g0) | DLRep(y1, x1 * g1)
+stmt.subproofs[0].set_simulated()
 
-v = nizk.verify()
+prover = stmt.get_prover()
+verifier = stmt.get_verifier()
 
-print(v)
+commitment = prover.commit()
+challenge = verifier.send_challenge(commitment)
+response = prover.compute_response(challenge)
+assert verifier.verify(response)
