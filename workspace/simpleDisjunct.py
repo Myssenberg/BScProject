3ba4c81121ddc9c3aa871e6ca5b5d1ce5aa6f291
Myssenberg <myje@itu.dk>
2023-03-29 16:06:48 +0200

    very simple nizk disjoint

--- workspace/simpleDisjunct.py
+++ workspace/simpleDisjunct.py
@@ -7,6 +7,28 @@ from petlib.ec import EcGroup
 
 group = EcGroup()
 
+g0 = group.generator()
+g1 = group.generator()
+
+x0 = Secret(value=3)
+x1 = Secret(value=1)
+
+y0 = x0.value * g0
+y1 = x1.value * g1
+
+stmt = DLRep(y0, x0 * g0) | DLRep(y1, x1 * g1)
+stmt.subproofs[1].set_simulated()
+
+nizk = stmt.prove({x0: 3})
+v = stmt.verify(nizk)
+print(v)
+
+
+
+
+"""
+group = EcGroup()
+
 g0 = group.hash_to_point(b"one")
 g1 = group.hash_to_point(b"two")
 
@@ -26,3 +48,4 @@ commitment = prover.commit()
 challenge = verifier.send_challenge(commitment)
 response = prover.compute_response(challenge)
 assert verifier.verify(response)
+"""
