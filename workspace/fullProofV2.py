7e3e42db035d4495c4c2b2b2901f8112153585c8
Myssenberg <myje@itu.dk>
2023-05-09 19:45:02 +0200

    full proof cleaned up a bit

--- workspace/fullProofV2.py
+++ workspace/fullProofV2.py
@@ -32,22 +32,22 @@ R1_r_lvs = Secret()
 R1_v.value = 1
 R1_r_v.value = order.random()
 
-start = time.process_time()
-R2_c0, R2_c1 = enc(g, pk_T, R1_v.value, R1_r_v.value)
-time_R1_enc1 = time.process_time() - start
+start = time.process_time_ns()
+R1_c0, R1_c1 = enc(g, pk_T, R1_v.value, R1_r_v.value)
+time_R1_enc1 = time.process_time_ns() - start
 
-enc_stmt1 = DLRep(R2_c0, R1_r_v*g) & DLRep(R2_c1, R1_v*g + R1_r_v*pk_T)
+enc_stmt1 = DLRep(R1_c0, R1_r_v*g) & DLRep(R1_c1, R1_v*g + R1_r_v*pk_T)
 
 #Range proof for three candidates (vote is either 0, 1 or 2)
-R1_range_stmt = RangeStmt(R2_c1, g, pk_T, 0, 16, R1_v, R1_r_v)
+R1_range_stmt = RangeStmt(R1_c1, g, pk_T, 0, 16, R1_v, R1_r_v)
 
 #Second proof of encryption
-R1_lv.value = order.random() #this should maybe be something else? A list or something
+R1_lv.value = order.random()
 R1_r_lv.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 c2, c3 = enc(g, pk_vs, R1_lv.value, R1_r_lv.value)
-time_R1_enc2 = time.process_time() - start
+time_R1_enc2 = time.process_time_ns() - start
 
 enc_stmt2 = DLRep(c2, R1_r_lv*g) & DLRep(c3, R1_lv*g + R1_r_lv*pk_vs)
 
@@ -56,9 +56,9 @@ c4, c5 = enc(g, pk_vs, 0, order.random()) #don't need to know these values for l
 
 R1_r_lvs.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 reEnc = re_enc(g, pk_vs, (c4, g+c5), R1_r_lvs.value)
-time_R1_reenc = time.process_time() - start
+time_R1_reenc = time.process_time_ns() - start
 
 c4Prime, c5Prime = reEnc
 
@@ -106,9 +106,9 @@ R2_ct_i = (R2_c0_i, R2_c1_i)
 #First proof of re-encryption, ct_v
 R2_r_v.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 R2_reEnc1 = re_enc(g, pk_T, (R2_c0_v, R2_c1_v), R2_r_v.value)
-time_R2_reenc1 = time.process_time() - start
+time_R2_reenc1 = time.process_time_ns() - start
 
 R2_c0_v_Prime, R2_c1_v_Prime = R2_reEnc1
 
@@ -122,9 +122,9 @@ R2_c0 = R2_c0_lv-R2_c0_lvs
 R2_c1 = R2_c1_lv-R2_c1_lvs
 R2_ct = (R2_c0, R2_c1)
 
-start = time.process_time()
+start = time.process_time_ns()
 R2_m_dec = dec(R2_ct, R2_sk.value)
-time_R2_dec = time.process_time() - start
+time_R2_dec = time.process_time_ns() - start
 
 one = Secret(name="one", value=1)
 R2_neg_c0 = (-1)*R2_c0
@@ -135,9 +135,9 @@ R2_dec_stmt = DLRep(0*g, one*R2_c1 + R2_sk*R2_neg_c0)
 
 R2_r_lv.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 R2_reEnc2 = re_enc(g, pk_vs, R2_ct_i, R2_r_lv.value)
-time_R2_reenc2 = time.process_time() - start
+time_R2_reenc2 = time.process_time_ns() - start
 
 R2_c0_lv_Prime, R2_c1_lv_Prime = R2_reEnc2
 
@@ -149,9 +149,9 @@ R2_reenc_stmt2 = DLRep(R2_c0_lv_Prime, one*R2_c0_i + R2_r_lv*g) & DLRep(R2_c1_lv
 #Third proof of re-encryption
 R2_r_lvs.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 R2_reEnc3 = re_enc(g, pk_vs, R2_ct_i, R2_r_lvs.value)
-time_R2_reenc3 = time.process_time() - start
+time_R2_reenc3 = time.process_time_ns() - start
 
 R2_c0_lvs_Prime, R2_c1_lvs_Prime = R2_reEnc3
 
@@ -175,7 +175,7 @@ R3_c0_v, R3_c1_v = enc(g, pk_T, 2, order.random())
 
 
 lv = bn.Bn(211).random()
-lvs = bn.Bn(211).random() #how do I make sure that m>1?
+lvs = bn.Bn(211).random()
 if lvs > lv:
     lvs = lv-1
 
@@ -189,12 +189,12 @@ R3_c1_i = 2*R3_c1_lvs
 R3_ct_i = (R3_c0_i, R3_c1_i)
 
 
-#First proof of re-encryption, ct_v
+#First proof of re-encryption
 R3_r_v.value = order.random()
 
-start = time.process_time()
-R3_reEnc1 = re_enc(g, pk_T, (R3_c0_v, R3_c1_v), R3_r_v.value) #PK_T here
-time_R3_reenc1 = time.process_time() - start
+start = time.process_time_ns()
+R3_reEnc1 = re_enc(g, pk_T, (R3_c0_v, R3_c1_v), R3_r_v.value)
+time_R3_reenc1 = time.process_time_ns() - start
 
 R3_c0_v_Prime, R3_c1_v_Prime = R3_reEnc1
 
@@ -204,13 +204,13 @@ R3_reenc_stmt1 = DLRep(R3_c0_v_Prime, one*R3_c0_v + R3_r_v*g) & DLRep(R3_c1_v_Pr
 
 #Proof of Decryption
 R3_sk.value = sk_vs
-R3_c0 = R3_c0_lv.pt_add(R3_c0_lvs.pt_neg())  #c0_lv.mod_sub(c0_lvs, order) #c0_lv+(-c0_lvs) #c0_lv+(-1)*c0_lvs  #c0_lv-c0_lvs
-R3_c1 = R3_c1_lv.pt_add(R3_c1_lvs.pt_neg())  #c1_lv.mod_sub(c1_lvs, order) #c1_lv+(-c1_lvs) #c1_lv+(-1)*c1_lvs  #c1_lv-c1_lvs
+R3_c0 = R3_c0_lv - R3_c0_lvs
+R3_c1 = R3_c1_lv - R3_c1_lvs
 R3_ct = (R3_c0, R3_c1)
 
-start = time.process_time()
+start = time.process_time_ns()
 R3_m_dec = dec(R3_ct, R3_sk.value)
-time_R3_dec = time.process_time() - start
+time_R3_dec = time.process_time_ns() - start
 
 one = Secret(name="one", value=1)
 R3_neg_c0 = (-1)*R3_c0
@@ -231,9 +231,9 @@ R3_range_stmt = RangeOnlyStmt(1, 1000, m)
 
 R3_r_lv.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 R3_reEnc2 = re_enc(g, pk_vs, R3_ct_i, R3_r_lv.value)
-time_R3_reenc2 = time.process_time() - start
+time_R3_reenc2 = time.process_time_ns() - start
 
 R3_c0_lv_Prime, R3_c1_lv_Prime = R3_reEnc2
 
@@ -245,9 +245,9 @@ R3_reenc_stmt2 = DLRep(R3_c0_lv_Prime, one*R3_c0_i + R3_r_lv*g) & DLRep(R3_c1_lv
 #Third proof of re-encryption
 R3_r_lvs.value = order.random()
 
-start = time.process_time()
+start = time.process_time_ns()
 R3_reEnc3 = re_enc(g, pk_vs, R3_ct_i, R3_r_lvs.value)
-time_R3_reenc3 = time.process_time() - start
+time_R3_reenc3 = time.process_time_ns() - start
 
 R3_c0_lvs_Prime, R3_c1_lvs_Prime = R3_reEnc3
 
@@ -269,15 +269,13 @@ stmt = relation1 | relation2 | relation3
 stmt.subproofs[1].set_simulated()
 stmt.subproofs[2].set_simulated()
 
-start = time.process_time()
-nizk = stmt.prove({R1_v: R1_v.value, R1_r_v: R1_r_v.value, R1_lv: R1_lv.value, R1_r_lv: R1_r_lv.value, R1_r_lvs: R1_r_lvs.value, R1_sk: R1_sk.value,
-                   R2_r_v: 0, R2_r_lv: 0, R2_r_lvs: 0, R2_sk: 0,
-                   R3_r_v: 0, R3_r_lv: 0, R3_r_lvs: 0, R3_sk: 0})
-time_R1_prove = time.process_time() - start
+start = time.process_time_ns()
+nizk = stmt.prove({R1_v: R1_v.value, R1_r_v: R1_r_v.value, R1_lv: R1_lv.value, R1_r_lv: R1_r_lv.value, R1_r_lvs: R1_r_lvs.value, R1_sk: R1_sk.value})
+time_R1_prove = time.process_time_ns() - start
 
-start = time.process_time()
+start = time.process_time_ns()
 v = stmt.verify(nizk)
-time_R1_verify = time.process_time() - start
+time_R1_verify = time.process_time_ns() - start
 
 print("Proof verified:", v)
 
@@ -286,22 +284,20 @@ print("Ballot generation time:", ballot_generation_time)
 print("Ballot verification time:", time_R1_verify)
 
 """
-"""
 
+"""
 #Relation 2 proof
 stmt = relation1 | relation2 | relation3
 stmt.subproofs[0].set_simulated()
 stmt.subproofs[2].set_simulated()
 
-start = time.process_time()
-nizk = stmt.prove({R1_v: 0, R1_r_v: 0, R1_lv: 0, R1_r_lv: 0, R1_r_lvs: 0, R1_sk: 0,
-                   R2_r_v: R2_r_v.value, R2_r_lv: R2_r_lv.value, R2_r_lvs: R2_r_lvs.value, R2_sk: R2_sk.value,
-                   R3_r_v: 0, R3_r_lv: 0, R3_r_lvs: 0, R3_sk: 0})
-time_R2_prove = time.process_time()-start
+start = time.process_time_ns()
+nizk = stmt.prove({R2_r_v: R2_r_v.value, R2_r_lv: R2_r_lv.value, R2_r_lvs: R2_r_lvs.value, R2_sk: R2_sk.value})
+time_R2_prove = time.process_time_ns()-start
 
-start = time.process_time()
+start = time.process_time_ns()
 v = stmt.verify(nizk)
-time_R2_verify = time.process_time()-start
+time_R2_verify = time.process_time_ns()-start
 
 print("Proof verified:", v)
 
@@ -317,15 +313,13 @@ stmt = relation1 | relation2 | relation3
 stmt.subproofs[0].set_simulated()
 stmt.subproofs[1].set_simulated()
 
-start = time.process_time()
-nizk = stmt.prove({R1_v: 0, R1_r_v: 0, R1_lv: 0, R1_r_lv: 0, R1_r_lvs: 0, R1_sk: 0,
-                   R2_r_v: 0, R2_r_lv: 0, R2_r_lvs: 0, R2_sk: 0,
-                   R3_sk: R3_sk.value, R3_r_v: R3_r_v.value, R3_r_lv: R3_r_lv.value, R3_r_lvs: R3_r_lvs.value})
-time_R3_prove = time.process_time()-start
+start = time.process_time_ns()
+nizk = stmt.prove({R3_sk: R3_sk.value, R3_r_v: R3_r_v.value, R3_r_lv: R3_r_lv.value, R3_r_lvs: R3_r_lvs.value})
+time_R3_prove = time.process_time_ns()-start
 
-start = time.process_time()
+start = time.process_time_ns()
 v = stmt.verify(nizk)
-time_R3_verify = time.process_time()-start
+time_R3_verify = time.process_time_ns()-start
 
 print("Proof verified:", v)
 
@@ -334,3 +328,25 @@ print("Ballot randomization time (relation 3):", R3_ballot_randomization_time)
 print("Ballot verification time:", time_R3_verify)
 
 
+"""
+#Example proof with all witnesses
+stmt = relation1 | relation2 | relation3
+stmt.subproofs[1].set_simulated()
+stmt.subproofs[2].set_simulated()
+
+start = time.process_time_ns()
+nizk = stmt.prove({R1_v: R1_v.value, R1_r_v: R1_r_v.value, R1_lv: R1_lv.value, R1_r_lv: R1_r_lv.value, R1_r_lvs: R1_r_lvs.value, R1_sk: R1_sk.value,
+                   R2_r_v: 0, R2_r_lv: 0, R2_r_lvs: 0, R2_sk: 0,
+                   R3_r_v: 0, R3_r_lv: 0, R3_r_lvs: 0, R3_sk: 0})
+time_R1_prove = time.process_time_ns() - start
+
+start = time.process_time_ns()
+v = stmt.verify(nizk)
+time_R1_verify = time.process_time_ns() - start
+
+print("Proof verified:", v)
+
+ballot_generation_time = time_R1_enc1 + time_R1_enc2 + time_R1_reenc + time_R1_prove
+print("Ballot generation time:", ballot_generation_time)
+print("Ballot verification time:", time_R1_verify)
+"""
