bdf29c1674d0cbb7e7f405e040088c5a72127a97
Myssenberg <myje@itu.dk>
2023-05-14 13:15:34 +0200

    documentation for elgamal added

--- workspace/elGamal.py
+++ workspace/elGamal.py
@@ -1,9 +1,45 @@
+"""Elliptic Curve ElGamal implementation
+
+This file contains an Elliptic Curve ElGamal implementation,
+containing group and key generation, and functions for encryption,
+decryption and re-encryption.
+
+The functions in this file are imported and used for the proof related
+implementations in other files of this project.
+
+This file requires that the environment you are running on have the 'petlib'
+library installed, which comes with an installation of the 'ZKSK' library
+needed for the other files in this project.
+
+The file contains the following functions:
+    - keygen: returns the group generator, order, and a key pair
+    - enc: returns an EC elgamal encrypted ciphertext
+    - dec: returns an EC elgamal decrypted message
+    - re_enc: returns an EC elgamal re-encrypted ciphertext
+"""
+
 from petlib.ec import EcGroup
 
-def keygen(): #inspiration from https://github.com/gdanezis/petlib/blob/master/examples/AHEG.py
+def keygen():
+    """Generates an EC group, generator, order and key pair
+
+    Args:
+        no arguments
+    
+    Returns:
+        g (EcPt): group generator
+        order (Bn): group order
+        pk (EcPt): public key
+        sk (Bn): secret key
+    """
+    
+    #Using the petlib library group operations to generate group
+    #and group values
     group = EcGroup()
     g = group.generator()
     order = group.order()
+
+    #Generating secret key at random from the EC group
     sk = order.random()
     pk = sk * g
     
@@ -11,16 +47,38 @@ def keygen(): #inspiration from https://github.com/gdanezis/petlib/blob/master/e
 
 
 def enc(g, pk, m, r):
-    #Elliptic curve so g**m * pk**r becomes m*g + r*pk
+    """Encryption of a message
+
+    Args:
+        g (EcPt): group generator
+        pk (EcPt): public key of the receiver
+        m (Bn) or (int): message to be encrypted
+        r (Bn) : randomness
+    
+    Returns:
+        (c0,c1) (EcPt, EcPt): ciphertext of encrypted message (c0, c1),
+                              where c0 = r*g and c1 = m*g + r*pk
+    """
+    
+    #Elliptic Curve so g**m * pk**r becomes m*g + r*pk
     c0 = r*g
     c1 = m*g + r*pk
 
     return (c0, c1)
 
-def dec(c, sk):
-    c0, c1 = c
-    print(c0)
-    print(sk)
+def dec(ct, sk):
+    """Decryption of a ciphertext
+
+    Args:
+        ct (EcPt, EcPt): ciphertext of encrypted message (c0, c1),
+                        where c0 = r*g and c1 = m*g + r*pk
+        sk (Bn): secret key / decryption key
+    
+    Returns:
+        message (EcPt): decrypted message, m, on the form m*g
+    """
+
+    c0, c1 = ct
 
     message = (c1 + (-sk*c0))
 
@@ -28,10 +86,26 @@ def dec(c, sk):
 
 
 
-def re_enc(g, h, ct, r):
+def re_enc(g, pk, ct, r):
+    """Re-Encryption of a ciphertext
+
+    Args:
+        g (EcPt): group generator
+        pk (EcPt): public key of the receiver
+        ct (EcPt, EcPt): ciphertext of encrypted message (c0, c1),
+                         where c0 = r*g and c1 = m*g + r*pk
+        r (Bn): randomness
+    
+    Returns:
+        (c0Prime, c1Prime) (EcPt, EcPt): ciphertext (c0Prime, c1Prime)
+                                         of re-encrypted ciphertext (c0, c1),
+                                         where c0 = r*g and c1 = m*g + r*pk and,
+                                         c0Prime = c0 + r*g and c1Prime = c1 + r*pk
+    """
+    
     c0, c1 = ct
 
     c0Prime = c0 + r*g
-    c1Prime = c1 + r*h
+    c1Prime = c1 + r*pk
 
     return (c0Prime, c1Prime)
